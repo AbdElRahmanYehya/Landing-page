ef5f9a62165b8aaef197d3706662e709bbec8473
AbdElRahmanYehya <a.yehya.aboelella@gmail.com>
2021-04-13 08:28:52 +0200

    v4
    working with the 3 required functionalities

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var sections = document.getElementsByTagName('section');
 
 for(let i = 0; i<sections.length; i++)
 {
-	document.getElementById("navigation-menu").insertAdjacentHTML('beforeend', '<ul id="'+sections[i].id+'-nav" onclick="myFunction(this)">'+sections[i].id+'</ul>');
+	document.getElementById("navigation-menu").insertAdjacentHTML('beforeend', '<li id="'+sections[i].id+'-nav" onclick="myFunction(this)" class="nav-item nav-link active">'+sections[i].id+'</li>');
 }
 
 
@@ -22,10 +22,33 @@ function myFunction(element) {
 	document.getElementById(element.textContent).scrollIntoView({behavior: "smooth"});
 }
 
-var highlight = document.querySelector("#highlight");
 
-window.addEventListener('scroll', function(e){
-    var y = window.scrollY;
-    var offset = y % 30;
-    highlight.style.marginTop =  - y % 30 + "px";
+// var rect = document.getElementById('about').getBoundingClientRect();
+// console.log(rect.top);
+
+document.addEventListener('scroll', function() {
+  // lastKnownScrollPosition = window.scrollY;
+
+  // console.log(lastKnownScrollPosition);
+  let max = Number.POSITIVE_INFINITY;
+	for(let i = 0; i<sections.length; i++)
+	{
+    	rect = document.getElementById(sections[i].id).getBoundingClientRect();
+
+	  	if(rect.top < 150 && rect.bottom > 150)
+	  	{
+	  		document.getElementById(sections[i].id).style.textShadow = "black 2px 0px 10px";
+	  		document.getElementById(sections[i].id).style.borderRadius = "30px";
+	  		document.getElementById(sections[i].id).style.border = "5px solid white";
+	  	}
+	  	else
+	  	{
+	  		document.getElementById(sections[i].id).style.removeProperty("background-color");
+	  		document.getElementById(sections[i].id).style.removeProperty("text-shadow");
+	  		document.getElementById(sections[i].id).style.removeProperty("border-radius");
+	  		document.getElementById(sections[i].id).style.removeProperty("border");
+	  	}
+	}
+
 });
+
